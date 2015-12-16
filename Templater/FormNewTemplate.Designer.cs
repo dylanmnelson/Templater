8fe92264262dc7f9f4b48b39fb55fac724213c57
Dylan Nelson <dylanmnelson@yahoo.com.au>
2015-12-16 15:39:36 +1100

    Updated New Template form UI with input fields for custom JS and CSS files

--- Templater/FormNewTemplate.Designer.cs
+++ Templater/FormNewTemplate.Designer.cs
@@ -29,15 +29,23 @@
         private void InitializeComponent()
         {
             this.groupBoxSelectTemplate = new System.Windows.Forms.GroupBox();
+            this.labelSelectStandard = new System.Windows.Forms.Label();
             this.radioButtonXHTML = new System.Windows.Forms.RadioButton();
             this.radioButtonHTML4 = new System.Windows.Forms.RadioButton();
             this.radioButtonHTML5 = new System.Windows.Forms.RadioButton();
             this.buttonCreateTemplate = new System.Windows.Forms.Button();
             this.groupBoxSetTitle = new System.Windows.Forms.GroupBox();
             this.textBoxTitle = new System.Windows.Forms.TextBox();
-            this.labelSelectStandard = new System.Windows.Forms.Label();
+            this.groupBoxCSSFilename = new System.Windows.Forms.GroupBox();
+            this.groupBoxJSFilename = new System.Windows.Forms.GroupBox();
+            this.textBoxCSSFilename = new System.Windows.Forms.TextBox();
+            this.labelCSSFilename = new System.Windows.Forms.Label();
+            this.labelJSFilename = new System.Windows.Forms.Label();
+            this.textBoxJSFilename = new System.Windows.Forms.TextBox();
             this.groupBoxSelectTemplate.SuspendLayout();
             this.groupBoxSetTitle.SuspendLayout();
+            this.groupBoxCSSFilename.SuspendLayout();
+            this.groupBoxJSFilename.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBoxSelectTemplate
@@ -46,22 +54,32 @@
             this.groupBoxSelectTemplate.Controls.Add(this.radioButtonXHTML);
             this.groupBoxSelectTemplate.Controls.Add(this.radioButtonHTML4);
             this.groupBoxSelectTemplate.Controls.Add(this.radioButtonHTML5);
-            this.groupBoxSelectTemplate.Location = new System.Drawing.Point(10, 10);
-            this.groupBoxSelectTemplate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBoxSelectTemplate.Location = new System.Drawing.Point(13, 12);
+            this.groupBoxSelectTemplate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBoxSelectTemplate.Name = "groupBoxSelectTemplate";
-            this.groupBoxSelectTemplate.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBoxSelectTemplate.Size = new System.Drawing.Size(427, 101);
+            this.groupBoxSelectTemplate.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBoxSelectTemplate.Size = new System.Drawing.Size(569, 124);
             this.groupBoxSelectTemplate.TabIndex = 0;
             this.groupBoxSelectTemplate.TabStop = false;
             this.groupBoxSelectTemplate.Text = "Select a standard";
             // 
+            // labelSelectStandard
+            // 
+            this.labelSelectStandard.AutoSize = true;
+            this.labelSelectStandard.Location = new System.Drawing.Point(77, 94);
+            this.labelSelectStandard.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelSelectStandard.Name = "labelSelectStandard";
+            this.labelSelectStandard.Size = new System.Drawing.Size(416, 17);
+            this.labelSelectStandard.TabIndex = 3;
+            this.labelSelectStandard.Text = "TIP: If you are unsure of what to select, HTML5 is recommended.";
+            // 
             // radioButtonXHTML
             // 
             this.radioButtonXHTML.AutoSize = true;
-            this.radioButtonXHTML.Location = new System.Drawing.Point(322, 34);
-            this.radioButtonXHTML.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioButtonXHTML.Location = new System.Drawing.Point(429, 42);
+            this.radioButtonXHTML.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.radioButtonXHTML.Name = "radioButtonXHTML";
-            this.radioButtonXHTML.Size = new System.Drawing.Size(62, 17);
+            this.radioButtonXHTML.Size = new System.Drawing.Size(76, 21);
             this.radioButtonXHTML.TabIndex = 2;
             this.radioButtonXHTML.TabStop = true;
             this.radioButtonXHTML.Text = "XHTML";
@@ -70,10 +88,10 @@
             // radioButtonHTML4
             // 
             this.radioButtonHTML4.AutoSize = true;
-            this.radioButtonHTML4.Location = new System.Drawing.Point(187, 34);
-            this.radioButtonHTML4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioButtonHTML4.Location = new System.Drawing.Point(249, 42);
+            this.radioButtonHTML4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.radioButtonHTML4.Name = "radioButtonHTML4";
-            this.radioButtonHTML4.Size = new System.Drawing.Size(61, 17);
+            this.radioButtonHTML4.Size = new System.Drawing.Size(75, 21);
             this.radioButtonHTML4.TabIndex = 1;
             this.radioButtonHTML4.TabStop = true;
             this.radioButtonHTML4.Text = "HTML4";
@@ -82,10 +100,10 @@
             // radioButtonHTML5
             // 
             this.radioButtonHTML5.AutoSize = true;
-            this.radioButtonHTML5.Location = new System.Drawing.Point(51, 34);
-            this.radioButtonHTML5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioButtonHTML5.Location = new System.Drawing.Point(68, 42);
+            this.radioButtonHTML5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.radioButtonHTML5.Name = "radioButtonHTML5";
-            this.radioButtonHTML5.Size = new System.Drawing.Size(61, 17);
+            this.radioButtonHTML5.Size = new System.Drawing.Size(75, 21);
             this.radioButtonHTML5.TabIndex = 0;
             this.radioButtonHTML5.TabStop = true;
             this.radioButtonHTML5.Text = "HTML5";
@@ -93,10 +111,10 @@
             // 
             // buttonCreateTemplate
             // 
-            this.buttonCreateTemplate.Location = new System.Drawing.Point(196, 190);
-            this.buttonCreateTemplate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonCreateTemplate.Location = new System.Drawing.Point(261, 314);
+            this.buttonCreateTemplate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonCreateTemplate.Name = "buttonCreateTemplate";
-            this.buttonCreateTemplate.Size = new System.Drawing.Size(56, 27);
+            this.buttonCreateTemplate.Size = new System.Drawing.Size(75, 33);
             this.buttonCreateTemplate.TabIndex = 1;
             this.buttonCreateTemplate.Text = "Create";
             this.buttonCreateTemplate.UseVisualStyleBackColor = true;
@@ -104,48 +122,99 @@
             // groupBoxSetTitle
             // 
             this.groupBoxSetTitle.Controls.Add(this.textBoxTitle);
-            this.groupBoxSetTitle.Location = new System.Drawing.Point(10, 115);
-            this.groupBoxSetTitle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBoxSetTitle.Location = new System.Drawing.Point(13, 142);
+            this.groupBoxSetTitle.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBoxSetTitle.Name = "groupBoxSetTitle";
-            this.groupBoxSetTitle.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBoxSetTitle.Size = new System.Drawing.Size(427, 58);
+            this.groupBoxSetTitle.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBoxSetTitle.Size = new System.Drawing.Size(569, 71);
             this.groupBoxSetTitle.TabIndex = 2;
             this.groupBoxSetTitle.TabStop = false;
             this.groupBoxSetTitle.Text = "Set a title";
             // 
             // textBoxTitle
             // 
-            this.textBoxTitle.Location = new System.Drawing.Point(23, 26);
-            this.textBoxTitle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxTitle.Location = new System.Drawing.Point(31, 32);
+            this.textBoxTitle.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxTitle.Name = "textBoxTitle";
-            this.textBoxTitle.Size = new System.Drawing.Size(384, 20);
+            this.textBoxTitle.Size = new System.Drawing.Size(511, 22);
             this.textBoxTitle.TabIndex = 0;
             // 
-            // labelSelectStandard
+            // groupBoxCSSFilename
             // 
-            this.labelSelectStandard.AutoSize = true;
-            this.labelSelectStandard.Location = new System.Drawing.Point(58, 76);
-            this.labelSelectStandard.Name = "labelSelectStandard";
-            this.labelSelectStandard.Size = new System.Drawing.Size(315, 13);
-            this.labelSelectStandard.TabIndex = 3;
-            this.labelSelectStandard.Text = "TIP: If you are unsure of what to select, HTML5 is recommended.";
+            this.groupBoxCSSFilename.Controls.Add(this.labelCSSFilename);
+            this.groupBoxCSSFilename.Controls.Add(this.textBoxCSSFilename);
+            this.groupBoxCSSFilename.Location = new System.Drawing.Point(13, 220);
+            this.groupBoxCSSFilename.Name = "groupBoxCSSFilename";
+            this.groupBoxCSSFilename.Size = new System.Drawing.Size(280, 71);
+            this.groupBoxCSSFilename.TabIndex = 3;
+            this.groupBoxCSSFilename.TabStop = false;
+            this.groupBoxCSSFilename.Text = "Set custom CSS filename";
+            // 
+            // groupBoxJSFilename
+            // 
+            this.groupBoxJSFilename.Controls.Add(this.labelJSFilename);
+            this.groupBoxJSFilename.Controls.Add(this.textBoxJSFilename);
+            this.groupBoxJSFilename.Location = new System.Drawing.Point(302, 220);
+            this.groupBoxJSFilename.Name = "groupBoxJSFilename";
+            this.groupBoxJSFilename.Size = new System.Drawing.Size(280, 71);
+            this.groupBoxJSFilename.TabIndex = 4;
+            this.groupBoxJSFilename.TabStop = false;
+            this.groupBoxJSFilename.Text = "Set custom JS filename";
+            // 
+            // textBoxCSSFilename
+            // 
+            this.textBoxCSSFilename.Location = new System.Drawing.Point(31, 31);
+            this.textBoxCSSFilename.Name = "textBoxCSSFilename";
+            this.textBoxCSSFilename.Size = new System.Drawing.Size(188, 22);
+            this.textBoxCSSFilename.TabIndex = 0;
+            // 
+            // labelCSSFilename
+            // 
+            this.labelCSSFilename.AutoSize = true;
+            this.labelCSSFilename.Location = new System.Drawing.Point(231, 33);
+            this.labelCSSFilename.Name = "labelCSSFilename";
+            this.labelCSSFilename.Size = new System.Drawing.Size(33, 17);
+            this.labelCSSFilename.TabIndex = 1;
+            this.labelCSSFilename.Text = ".css";
+            // 
+            // labelJSFilename
+            // 
+            this.labelJSFilename.AutoSize = true;
+            this.labelJSFilename.Location = new System.Drawing.Point(233, 33);
+            this.labelJSFilename.Name = "labelJSFilename";
+            this.labelJSFilename.Size = new System.Drawing.Size(22, 17);
+            this.labelJSFilename.TabIndex = 6;
+            this.labelJSFilename.Text = ".js";
+            // 
+            // textBoxJSFilename
+            // 
+            this.textBoxJSFilename.Location = new System.Drawing.Point(28, 31);
+            this.textBoxJSFilename.Name = "textBoxJSFilename";
+            this.textBoxJSFilename.Size = new System.Drawing.Size(188, 22);
+            this.textBoxJSFilename.TabIndex = 5;
             // 
             // FormNewTemplate
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(446, 234);
+            this.ClientSize = new System.Drawing.Size(595, 368);
+            this.Controls.Add(this.groupBoxJSFilename);
+            this.Controls.Add(this.groupBoxCSSFilename);
             this.Controls.Add(this.groupBoxSetTitle);
             this.Controls.Add(this.buttonCreateTemplate);
             this.Controls.Add(this.groupBoxSelectTemplate);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormNewTemplate";
             this.Text = "New Template";
             this.groupBoxSelectTemplate.ResumeLayout(false);
             this.groupBoxSelectTemplate.PerformLayout();
             this.groupBoxSetTitle.ResumeLayout(false);
             this.groupBoxSetTitle.PerformLayout();
+            this.groupBoxCSSFilename.ResumeLayout(false);
+            this.groupBoxCSSFilename.PerformLayout();
+            this.groupBoxJSFilename.ResumeLayout(false);
+            this.groupBoxJSFilename.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -160,5 +229,11 @@
         private System.Windows.Forms.GroupBox groupBoxSetTitle;
         private System.Windows.Forms.TextBox textBoxTitle;
         private System.Windows.Forms.Label labelSelectStandard;
+        private System.Windows.Forms.GroupBox groupBoxCSSFilename;
+        private System.Windows.Forms.Label labelCSSFilename;
+        private System.Windows.Forms.TextBox textBoxCSSFilename;
+        private System.Windows.Forms.GroupBox groupBoxJSFilename;
+        private System.Windows.Forms.Label labelJSFilename;
+        private System.Windows.Forms.TextBox textBoxJSFilename;
     }
 }
