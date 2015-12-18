6318c9a1d35287634dded869868652902ead72db
Dylan Nelson <dylanmnelson@yahoo.com.au>
2015-12-18 12:37:35 +1100

    Added application icon

--- Templater/FormMain.Designer.cs
+++ Templater/FormMain.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
             this.menuStripMain = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.newTemplateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -215,6 +216,7 @@
             this.ClientSize = new System.Drawing.Size(851, 567);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.menuStripMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStripMain;
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MinimumSize = new System.Drawing.Size(598, 47);
